9e7b37e22eb1ea65641c76239d2eb3ad2e5d8073
Benoist Bouteiller <b@bouteiller.contact>
2022-06-10 00:27:14 +0200

    fix(*): same width

--- hooks/useIsMobile.tsx
+++ hooks/useIsMobile.tsx
@@ -1,23 +1,23 @@
 import { useState, useEffect } from 'react';
 
-export default function useIsMobile(){
-    const [isMobile, setIsMobile] = useState(false);
-    function handleResize() {
-      if (window.innerWidth <= 600) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
+export default function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+  function handleResize() {
+    if (window.innerWidth <= 600) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
     }
-  
-    useEffect(() => {
-      window.addEventListener("resize", handleResize);
-      if (window.innerWidth <= 600) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-    }, []);
+  }
 
-    return isMobile;
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    if (window.innerWidth <= 600) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  }, []);
+
+  return isMobile;
 }
--- pages/slider/[token].tsx
+++ pages/slider/[token].tsx
@@ -1,16 +1,15 @@
-import Slider from "../../Page-Related/Slider/Slider";
+import Slider from '../../Page-Related/Slider/Slider';
 
 interface props {
-    readonly token: string
+  readonly token: string;
 }
 
-export default function slider(props:props) {
+export default function slider(props: props) {
   return <Slider {...props} />;
 }
 
 export async function getServerSideProps({ query, req }) {
-    return {
-      props: { token: query.token},
-    };
-  }
-  
+  return {
+    props: { token: query.token },
+  };
+}
