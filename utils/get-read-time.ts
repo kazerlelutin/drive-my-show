4795e9b972965918ff22f410c0f6d7d30e661e05
Kazerlelutin <b@bouteiller.contact>
2023-01-08 17:25:18 +0100

    fix(get-read-time): style

--- utils/get-read-time.ts
+++ utils/get-read-time.ts
@@ -3,5 +3,5 @@ export function getReadTime(text: string): string {
   const noOfWords = text.split(/\s/g).length
   const minutes = noOfWords / wordsPerMinute
   const readTime = Math.ceil(minutes)
-  return `~ ${readTime} min`
+  return `${readTime}`
 }
