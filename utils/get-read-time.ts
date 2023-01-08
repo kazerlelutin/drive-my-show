d6d8d3215bd4339728203d0ff2d100fe931da436
Kazerlelutin <b@bouteiller.contact>
2023-01-08 17:28:31 +0100

    fix(read): change speed

--- utils/get-read-time.ts
+++ utils/get-read-time.ts
@@ -1,5 +1,5 @@
 export function getReadTime(text: string): string {
-  const wordsPerMinute = 200
+  const wordsPerMinute = 120
   const noOfWords = text.split(/\s/g).length
   const minutes = noOfWords / wordsPerMinute
   const readTime = Math.ceil(minutes)
